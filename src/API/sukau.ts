7e4453545c6a3e30dd1e70c96e18310218de1765
Haziq Zikry <haziqzikry@gmail.com>
2023-03-15 17:29:53 +0800

    Revert "Merge branch 'ape' into sukau"
    This reverts commit 412cf79ed2ac532c98fe146482373ac45b42199b, reversing
    changes made to a9321e89d462df075f409c438699d0de4068306b.

--- src/API/sukau.ts
+++ src/API/sukau.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/sukau/";
+const API_URL = "http://localhost:5000/forms/";
 
-const { alertError, alertSuccess, alertInfo, alertWarning } = useToast();
 const SukauAPI = {
-  async viewHistory(sukauId: string) {
-    const response = await axios.get(API_URL + sukauId);
-    return response.data;
+  async getAllOrders() {
+    try {
+      const response = await axios.get(API_URL);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
   },
 
   // async getOrderById(){
