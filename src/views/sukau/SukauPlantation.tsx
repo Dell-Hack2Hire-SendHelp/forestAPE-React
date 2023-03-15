4533fa34cd337605a7d2d97dcd991f1123217057
Haziq Zikry <haziqzikry@gmail.com>
2023-03-16 01:24:51 +0800

    Update SukauPlantation.tsx
    update plantation form

--- src/views/sukau/SukauPlantation.tsx
+++ src/views/sukau/SukauPlantation.tsx
@@ -6,137 +6,131 @@ import { FaSeedling } from "react-icons/fa";
 import { useToast } from "../../hooks/useToast";
 
 export default function SukauPlantation() {
-	return (
-		<>
-			<PlantingForm />
-		</>
-	);
+  return (
+    <>
+      <PlantingForm />
+    </>
+  );
 }
 
 function PlantingForm() {
-	function getOrder() {
-		//TODO: Get single order from database by id
-	}
-	const navigateTo = useNavigate();
-	const [order, setOrder] = useState<any>([]);
-	const { alertSuccess, alertError } = useToast();
-	// useEffect(() => {}, []);
+  function getOrder() {
+    //TODO: Get single order from database by id
+  }
+  const navigateTo = useNavigate();
+  const [order, setOrder] = useState<any>([]);
+  const { alertSuccess, alertError } = useToast();
+  // useEffect(() => {}, []);
 
-	const SukauCertification = () => {
-		navigateTo("/sukau/certification");
-	};
+  const SukauCertification = () => {
+    navigateTo("/sukau/certification");
+  };
 
-	const handleError = (error: any) => {
-		alertError("Please fill in the required fields");
-	};
+  const handleError = (error: any) => {
+    alertError("Please fill in the required fields");
+  };
 
-	return (
-		<>
-			<Box className='flex flex-col items-center justify-center'>
-				<Typography
-					variant='h4'
-					className='font-["Cantora_One"] flex '
-					color='primary'
-				>
-					Plantation 1 <FaSeedling />
-				</Typography>
-			</Box>
-			<hr />
-			<Box className='flex flex-col items-center justify-center'>
-				<FormContainer
-					onSuccess={SukauCertification}
-					onError={handleError}
-					defaultValues={{
-						//TODO: Get single order from database by id
+  return (
+    <>
+      <Box className="flex flex-col items-center justify-center">
+        <Typography
+          variant="h4"
+          className='font-["Cantora_One"] flex '
+          color="primary"
+        >
+          Plantation 1 <FaSeedling />
+        </Typography>
+      </Box>
+      <hr />
+      <Box className="flex flex-col items-center justify-center">
+        <FormContainer
+          onSuccess={SukauCertification}
+          onError={handleError}
+          defaultValues={{
+            //TODO: Get single order from database by id
 
-						customer: {
-							"default-text-field": "Ethan",
-						},
-						date: {
-							"default-text-field": "20/12/2023",
-						},
+            customer: {
+              "default-text-field": "Ethan",
+            },
+            date: {
+              "default-text-field": "20/12/2023",
+            },
 
-						tree_number: {
-							"default-text-field": "4",
-						},
-					}}
-				>
-					<Box className='flex flex-col gap-3 mt-5'>
-						<TextFieldElement
-							name={"customer.default-text-field"}
-							label='Receiver Name'
-							variant='outlined'
-							size='small'
-							disabled
-						/>
-						<TextFieldElement
-							name={"date.default-text-field"}
-							label='Order Date'
-							variant='outlined'
-							size='small'
-							disabled
-						/>
-						<TextFieldElement
-							name={"tree_number.default-text-field"}
-							label='Number Planted Tree/s'
-							variant='outlined'
-							size='small'
-							disabled
-						/>
-						Coordinates
-						<Grid container spacing={2}>
-							<Grid item xs={6}>
-								<TextFieldElement
-									type={"number"}
-									required
-									fullWidth
-									variant='outlined'
-									label='Longitude'
-									name='Longitude'
-									validation={{
-										required:
-											"Longitude is required, only number is allowed",
-									}}
-								/>
-							</Grid>
-							<Grid item xs={6}>
-								<TextFieldElement
-									type={"number"}
-									required
-									fullWidth
-									variant='outlined'
-									label='Latitude'
-									name='Latitude'
-									validation={{
-										required:
-											"Latitude is required, only number is allowed",
-									}}
-								/>
-							</Grid>
-							<Grid item xs={12}></Grid>
-						</Grid>
-						<Typography
-							variant='h6'
-							className='font-["Cantora_One"] underline'
-							color='primary'
-						>
-							Upload Plantation Image
-						</Typography>
-						<input type='file' required />
-					</Box>
+            tree_number: {
+              "default-text-field": "4",
+            },
+          }}
+        >
+          <Box className="flex flex-col gap-3 mt-5">
+            <TextFieldElement
+              name={"customer.default-text-field"}
+              label="Receiver Name"
+              variant="outlined"
+              size="small"
+              disabled
+            />
+            <TextFieldElement
+              name={"date.default-text-field"}
+              label="Order Date"
+              variant="outlined"
+              size="small"
+              disabled
+            />
+            <TextFieldElement
+              name={"tree_number.default-text-field"}
+              label="Number Planted Tree/s"
+              variant="outlined"
+              size="small"
+              disabled
+            />
+            Coordinates
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <TextFieldElement
+                  type={"number"}
+                  required
+                  fullWidth
+                  variant="outlined"
+                  label="Longitude"
+                  name="Longitude"
+                  validation={{
+                    required: "Longitude is required",
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextFieldElement
+                  type={"number"}
+                  required
+                  fullWidth
+                  variant="outlined"
+                  label="Latitude"
+                  name="Latitude"
+                  validation={{
+                    required: "Latitude is required",
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}></Grid>
+            </Grid>
+            <Typography
+              variant="h6"
+              className='font-["Cantora_One"] underline'
+              color="primary"
+            >
+              Upload Plantation Image
+            </Typography>
+            <input type="file" required />
+          </Box>
 
-					<div className='flex flex-col items-center h-screen'>
-						<br></br>
-						<Button
-							type='submit'
-							variant='contained'
-							color='primary'
-						>
-							Save
-						</Button>
-					</div>
-				</FormContainer>
-			</Box>
-		</>
-	);
+          <div className="flex flex-col items-center h-screen">
+            <br></br>
+            <Button type="submit" variant="contained" color="primary">
+              Save
+            </Button>
+          </div>
+        </FormContainer>
+      </Box>
+    </>
+  );
 }
