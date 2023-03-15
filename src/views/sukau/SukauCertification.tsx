da25f56587e632fc71ff1726a26be2b3ddbc02d8
Haziq Zikry <haziqzikry@gmail.com>
2023-03-15 23:51:30 +0800

    sukau certification page

--- src/views/sukau/SukauCertification.tsx
+++ src/views/sukau/SukauCertification.tsx
@@ -10,7 +10,11 @@ import {
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import SukauAPI from "../../API/sukau";
-import PDFViewer from "../../components/PDFViewer";
+import React from "react";
+import { MdHistoryEdu } from "react-icons/md";
+import { PDFViewer, PDFDownloadLink } from "@react-pdf/renderer";
+import PlantingCertificate from "../../PlantingCertificate";
+import ThankYouCertificate from "../../ThankYouCertificate";
 
 export default function Home() {
   return (
@@ -21,25 +25,26 @@ export default function Home() {
 }
 
 function Certificate() {
-  const navigateTo = useNavigate();
-  const [order, setOrder] = useState<any>([]);
-
-  // useEffect(() => {
-  // const res = SukauAPI.getAllOrders().then((res: { data: any }) => {
-  //   setOrder(res.data);
-  // });
-  // }, []);
-
-  // const SukauPlantation = () => {
-  //   navigateTo("/sukau/SukauPlantation");
-  // };
-
   return (
     <>
-      Here is your certifcate!
-      <div>
-        <PDFViewer url="" />
-      </div>
+      <Box className="flex flex-col items-center justify-center mt-2">
+        <Typography
+          variant="h4"
+          className='font-["Cantora_One"] flex '
+          color="primary"
+        >
+          Thank you! <MdHistoryEdu />
+        </Typography>
+      </Box>
+      <hr />
+      <Box className="items-center justify-center mt-2" sx={{ width: "100%" }}>
+        <Box>
+          <PDFViewer style={{ width: "100%", minHeight: "100vh" }}>
+            <ThankYouCertificate name="Soh Jun Wei" numberOfTrees={10} />
+          </PDFViewer>
+        </Box>
+      </Box>
+      <hr />
     </>
   );
 }
--- src/views/sukau/SukauPlantation.tsx
+++ src/views/sukau/SukauPlantation.tsx
@@ -1,12 +1,9 @@
-import {
-  Box,
-  Button,
-  Grid, TextField, Typography
-} from "@mui/material";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { FormContainer, TextFieldElement } from "react-hook-form-mui";
 import { useNavigate } from "react-router-dom";
-import {FaSeedling} from "react-icons/fa";
+import { FaSeedling } from "react-icons/fa";
+import { useToast } from "../../hooks/useToast";
 
 export default function SukauPlantation() {
   return (
@@ -19,107 +16,115 @@ export default function SukauPlantation() {
 function PlantingForm() {
   function getOrder() {
     //TODO: Get single order from database by id
-
-
-    const navigateTo = useNavigate();
-    const [order, setOrder] = useState<any>([]);
-
-    useEffect(() => {
-    
-    }, []);
-
-    const SukauCertification = () => {
-      navigateTo("/sukau/SukauCertification");
-    };
   }
+  const navigateTo = useNavigate();
+  const [order, setOrder] = useState<any>([]);
+  //   const { alertSuccess, alertError } = useToast();
+  // useEffect(() => {}, []);
+
+  const SukauCertification = () => {
+    //     if (Latitude == null && Longitude == null) {
+    //       alertError("Please enter the coordinates");
+    //     }
+
+    navigateTo("/sukau/certification");
+  };
 
   return (
-		<>
-			<Box className='flex flex-col items-center justify-center mt-2'>
-				<Typography
-					variant='h4'
-					className='font-["Cantora_One"] flex '
-					color='primary'
-				>
-					Plantation 1 <FaSeedling />
-				</Typography>
-			</Box>
-			<hr />
-			<Box width={"60%"} >
-				<FormContainer
-					defaultValues={{
-						//TODO: Get single order from database by id
+    <>
+      <Box className="flex flex-col items-center justify-center">
+        <Typography
+          variant="h4"
+          className='font-["Cantora_One"] flex '
+          color="primary"
+        >
+          Plantation 1 <FaSeedling />
+        </Typography>
+      </Box>
+      <hr />
+      <Box className="flex flex-col items-center justify-center">
+        <form>
+          <FormContainer
+            defaultValues={{
+              //TODO: Get single order from database by id
 
-						customer: {
-							"default-text-field": "Ethan",
-						},
-						date: {
-							"default-text-field": "20/12/2023",
-						},
+              customer: {
+                "default-text-field": "Ethan",
+              },
+              date: {
+                "default-text-field": "20/12/2023",
+              },
 
-						tree_number: {
-							"default-text-field": "4",
-						},
-					}}
-				>
-					<Box className='flex flex-col gap-3 mt-5'>
-						<TextFieldElement
-							name={"customer.default-text-field"}
-							label='Customer Name'
-							variant='outlined'
-							size='small'
-							disabled
-						/>
-						<TextFieldElement
-							name={"date.default-text-field"}
-							label='Order Date'
-							variant='outlined'
-							size='small'
-							disabled
-						/>
-						<TextFieldElement
-							name={"tree_number.default-text-field"}
-							label='Number Planted Tree/s'
-							variant='outlined'
-							size='small'
-							disabled
-						/>
-						Coordinates
-						<Grid container spacing={2}>
-							<Grid item xs={6}>
-								<TextField
-									fullWidth
-									variant='outlined'
-									label='Longitude'
-									name='Longitude'
-									required
-								/>
-							</Grid>
-							<Grid item xs={6}>
-								<TextField
-									fullWidth
-									variant='outlined'
-									label='Latitude'
-									name='Latitude'
-									required
-								/>
-							</Grid>
-							<Grid item xs={12}></Grid>
-						</Grid>
-						<Button variant='contained' component='label'>
-							Upload Image
-							<input type='file' required />
-						</Button>
-					</Box>
+              tree_number: {
+                "default-text-field": "4",
+              },
+            }}
+          >
+            <Box className="flex flex-col gap-3 mt-5">
+              <TextFieldElement
+                name={"customer.default-text-field"}
+                label="Receiver Name"
+                variant="outlined"
+                size="small"
+                disabled
+              />
+              <TextFieldElement
+                name={"date.default-text-field"}
+                label="Order Date"
+                variant="outlined"
+                size="small"
+                disabled
+              />
+              <TextFieldElement
+                name={"tree_number.default-text-field"}
+                label="Number Planted Tree/s"
+                variant="outlined"
+                size="small"
+                disabled
+              />
+              Coordinates
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <TextFieldElement
+                    required
+                    fullWidth
+                    variant="outlined"
+                    label="Longitude"
+                    name="Longitude"
+                    validation={{ required: "Username is required" }}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    variant="outlined"
+                    label="Latitude"
+                    name="Latitude"
+                  />
+                </Grid>
+                <Grid item xs={12}></Grid>
+              </Grid>
+              <Button variant="contained" component="label">
+                Upload Image
+                <input type="file" required />
+              </Button>
+            </Box>
 
-					<div className='flex flex-col items-center h-screen'>
-						<br></br>
-						<Button variant='contained' color='primary'>
-							save
-						</Button>
-					</div>
-				</FormContainer>
-			</Box>
-		</>
+            <div className="flex flex-col items-center h-screen">
+              <br></br>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={SukauCertification}
+              >
+                save
+              </Button>
+            </div>
+          </FormContainer>
+        </form>
+      </Box>
+    </>
   );
 }
