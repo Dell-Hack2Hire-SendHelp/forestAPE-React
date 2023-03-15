f7dbcbd5225d3345f65867c14fb6b980a8192f7d
Ethanlyt <ethanleong86@gmail.com>
2023-03-15 17:33:24 +0800

    Revert "Merge branch 'sukau' into ape"
    This reverts commit d5ff957edbdfe825abf263a4c9fe56d95d30239a, reversing
    changes made to 705213ed0e7c031dedd1a5efb6abef62d99fb987.

---
+++ src/views/customer/Home.tsx
@@ -0,0 +1,45 @@
+import {Box, Typography, Button, Card, CardActions, CardContent, CardMedia} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import {AiFillStar} from 'react-icons/ai';
+
+
+export default function Home() {
+    const navigateTo = useNavigate();
+    const viewItem = () => {
+			navigateTo("/customer/shop");
+		};
+    return (
+		<>
+			<Card className='text-center' sx={{ maxWidth: 345 }}>
+				<CardMedia />
+				<CardContent>
+					<Typography gutterBottom variant='h5' component='div'>
+						Plant A Tree
+					</Typography>
+					<Box
+						className='flex flex-row justify-center'
+						component='span'
+						sx={{ display: "block", p: 0.5, m: 1, borderRadius: 2 }}
+					>
+						<AiFillStar />
+						<AiFillStar />
+						<AiFillStar />
+						<AiFillStar />
+						<AiFillStar />
+					</Box>
+					<Box
+						component='span'
+						sx={{ display: "block", p: 0.5, m: 1, borderRadius: 2 }}
+					>
+						RM25.00
+					</Box>
+				</CardContent>
+				<CardActions style={{ justifyContent: "center" }}>
+					<Button size='small' variant='contained' onClick={viewItem}>
+						SELECT OPTIONS
+					</Button>
+				</CardActions>
+			</Card>
+		</>
+	);
+}
